f51ebb2ae81f31ab30361184987c2d4e958aaaf6
Mansoor C <mansoorchengat@gmail.com>
2020-07-18 14:22:27 +0000

    first commit

---
+++ shippy-service-consignment/go.mod
@@ -0,0 +1,8 @@
+module github.com/fab-man/shippy/shippy-service-consignment
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.30.0
+)
---
+++ shippy-service-consignment/main.go
@@ -0,0 +1,14 @@
+package main
+
+// Import the generated protobuf code
+
+import(
+	pb "github.com/fab-man/shippy/shippy-service-consignment/proto/consignment"
+)
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func ParseFile(file string) {}
+func main()                 {}
